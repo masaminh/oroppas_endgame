b38a704de40f3a6e26a97b518a7092ddd20e0348
Masami Asakawa <masami.askw@gmail.com>
2018-09-19 21:33:38 +0900

    αβ探索を最終盤用とそれ以外で関数を分ける (refs #17)

--- src/lib/logic.cc
+++ src/lib/logic.cc
@@ -13,7 +13,7 @@ namespace board = oroppas::endgame::board;
 namespace utility = oroppas::endgame::utility;
 
 ///
-/// αβ探索
+/// αβ探索 (最終盤用)
 /// @param[in] black 着手側ビットボード
 /// @param[in] white 相手側ビットボード
 /// @param[in] alpha 対象とする下限値
@@ -21,8 +21,8 @@ namespace utility = oroppas::endgame::utility;
 /// @param[in,out] benchmark ベンチマーク用情報
 /// @return 評価値
 ///
-int alphabeta(uint64_t black, uint64_t white, int alpha, int beta,
-              Benchmark *benchmark) {
+int alphabeta_leafside(uint64_t black, uint64_t white, int alpha, int beta,
+                       Benchmark *benchmark) {
   auto blank = ~(black | white);
   auto countBlank = utility::CountBits(blank);
   if (countBlank == 1) {
@@ -38,6 +38,57 @@ int alphabeta(uint64_t black, uint64_t white, int alpha, int beta,
     }
 
     return board::GetScore(black, white);
+  }
+
+  auto positions = board::GetMovableBitBoard(black, white);
+  if (positions == 0) {
+    if (board::GetMovableBitBoard(white, black) == 0) {
+      // 双方が石を置けない
+      ++benchmark->leaf;
+      return board::GetScore(black, white);
+    } else {
+      // パス
+      return -alphabeta_leafside(white, black, -beta, -alpha, benchmark);
+    }
+  }
+
+  ++benchmark->internal;
+
+  while (positions) {
+    auto position = uint64_t{1} << utility::CountNTZ(positions);
+
+    auto newb = black;
+    auto neww = white;
+    board::Move(position, &newb, &neww);
+
+    alpha = std::max(alpha,
+                     -alphabeta_leafside(neww, newb, -beta, -alpha, benchmark));
+
+    if (alpha >= beta) {
+      return alpha;
+    }
+
+    positions ^= position;
+  }
+
+  return alpha;
+}
+
+///
+/// αβ探索
+/// @param[in] black 着手側ビットボード
+/// @param[in] white 相手側ビットボード
+/// @param[in] alpha 対象とする下限値
+/// @param[in] beta 対象とする上限値
+/// @param[in,out] benchmark ベンチマーク用情報
+/// @return 評価値
+///
+int alphabeta(uint64_t black, uint64_t white, int alpha, int beta,
+              Benchmark *benchmark) {
+  auto blank = ~(black | white);
+  auto countBlank = utility::CountBits(blank);
+  if (countBlank <= 4) {
+    return alphabeta_leafside(black, white, alpha, beta, benchmark);
   }
 
   auto positions = board::GetMovableBitBoard(black, white);
@@ -76,8 +127,7 @@ int alphabeta(uint64_t black, uint64_t white, int alpha, int beta,
     positions ^= position;
   }
 
-  // 最終盤までソートするとそのコストの方が遅くなるので、一定のところでソートは止めておく
-  if (score_table_size > 1 && countBlank > 4) {
+  if (score_table_size > 1) {
     for (auto i = 0; i < score_table_size; ++i) {
       auto score = score_table + i;
       score->score = utility::CountBits(
